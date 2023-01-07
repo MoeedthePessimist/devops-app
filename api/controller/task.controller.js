73ed5885be006ad9694626f6f752d41eaa7a3553
Touqeer Hassan <touqeerhassan76@gmail.com>
2023-01-07 09:38:03 +0500

    added docker files

--- api/controller/task.controller.js
+++ api/controller/task.controller.js
@@ -1,26 +1,41 @@
-const taskService  = require('../service/task.service');
-const logger = require('../logger/api.logger');
+const taskService = require("../service/task.service");
+const logger = require("../logger/api.logger");
 
 class TodoController {
-
-    async getTasks() {
-        logger.info('Controller: getTasks')
-        return await taskService.getTasks();
+  async getTasks() {
+    try {
+      logger.info("Controller: getTasks");
+      return await taskService.getTasks();
+    } catch (e) {
+      logger.error(e);
     }
+  }
 
-    async createTask(task) {
-        logger.info('Controller: createTask', task);
-        return await taskService.createTask(task);
+  async createTask(task) {
+    try {
+      logger.info("Controller: createTask", task);
+      return await taskService.createTask(task);
+    } catch (e) {
+      logger.error(e);
     }
+  }
 
-    async updateTask(task) {
-        logger.info('Controller: updateTask', task);
-        return await taskService.updateTask(task);
+  async updateTask(task) {
+    try {
+      logger.info("Controller: updateTask", task);
+      return await taskService.updateTask(task);
+    } catch (e) {
+      logger.error(e);
     }
+  }
 
-    async deleteTask(taskId) {
-        logger.info('Controller: deleteTask', taskId);
-        return await taskService.deleteTask(taskId);
+  async deleteTask(taskId) {
+    try {
+      logger.info("Controller: deleteTask", taskId);
+      return await taskService.deleteTask(taskId);
+    } catch (e) {
+      logger.error(e);
     }
+  }
 }
 module.exports = new TodoController();
